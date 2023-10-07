455e5929511f972298e91a272412999b1f3a2e4f
Ajuarjun5007 <115939598+Ajuarjun5007@users.noreply.github.com>
2023-10-07 11:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,28 +57,4 @@ addBtn.addEventListener('click', function() {
 
 });
 
-for (const expense of expenses) {
-    totalAmount += expense.amount;
-    totalAmountCell.textContent = totalAmount;
 
-    // const newRow = expensesTableBody.inserRow();
-    // const categoryCell = newRow.insertCell();
-    // const amountCell = newRow.insertCell();
-    // const dateCell = newRow.insertCell();
-    // const deleteCell = newRow.insertCell();
-    // const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', function() {
-        expenses.splice(expenses.indexOf(expense), 1);
-
-        totalAmount -= expense.amount;
-        totalAmountCell.textContent = totalAmount;
-
-        expensesTableBody.removeChild(newRow);
-    });
-    categoryCell.textContent = expense.category;
-    amountCell.textContent = expense.amount;
-    dateCell.textContent = expense.date;
-    deleteCell.appendChild(deleteBtn);
-}
