f3325fccb94d7cd0f23e891c33168ec7869ada45
Ajuarjun5007 <115939598+Ajuarjun5007@users.noreply.github.com>
2023-10-07 10:52:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,12 +61,12 @@ for (const expense of expenses) {
     totalAmount += expense.amount;
     totalAmountCell.textContent = totalAmount;
 
-    const newRow = expensesTableBody.inserRow();
-    const categoryCell = newRow.insertCell();
-    const amountCell = newRow.insertCell();
-    const dateCell = newRow.insertCell();
-    const deleteCell = newRow.insertCell();
-    const deleteBtn = document.createElement('button');
+    // const newRow = expensesTableBody.inserRow();
+    // const categoryCell = newRow.insertCell();
+    // const amountCell = newRow.insertCell();
+    // const dateCell = newRow.insertCell();
+    // const deleteCell = newRow.insertCell();
+    // const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete';
     deleteBtn.classList.add('delete-btn');
     deleteBtn.addEventListener('click', function() {
